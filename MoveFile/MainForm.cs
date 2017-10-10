ed0901eeb5719e96e8b42239da18c7cad990f7e6
Owen <1787hz@gmail.com>
2017-10-10 11:05:43 +0800

    find source folder

--- MoveFile/MainForm.cs
+++ MoveFile/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoveFile.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,35 @@ namespace MoveFile
 {
     public partial class MainForm : Form
     {
+        Model _model;
+
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _model = new Model();
+        }
+
+        private void _sourceButton_Click(object sender, EventArgs e)
+        {
+            _folderBrowserDialog.ShowDialog();
+            _sourceTextBox.Text = _folderBrowserDialog.SelectedPath;
+            _model.SourcePath = _folderBrowserDialog.SelectedPath;
+        }
+
+        private void _goalButton_Click(object sender, EventArgs e)
+        {
+            _folderBrowserDialog.ShowDialog();
+            _goalTextBox.Text = _folderBrowserDialog.SelectedPath;
+            _model.GoalPath = _folderBrowserDialog.SelectedPath;
+        }
+
+        private void _doButton_Click(object sender, EventArgs e)
+        {
+            _model.DoOperate();
         }
     }
 }
---
+++ MoveFile/Models/Model.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveFile.Models
+{
+    class Model
+    {
+        string _sourcePath;
+        string _goalPath;
+
+        public string SourcePath { get => _sourcePath; set => _sourcePath = value; }
+        public string GoalPath { get => _goalPath; set => _goalPath = value; }
+
+        public void DoOperate()
+        {
+            FindFile();
+        }
+        private void FindFile()
+        {
+            string[] subDirectorys = Directory.GetDirectories(_sourcePath);
+            subDirectorys.ToList().ForEach(e => Console.WriteLine(e));
+        }
+    }
+}
